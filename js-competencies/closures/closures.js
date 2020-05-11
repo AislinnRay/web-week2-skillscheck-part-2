17e1c03b6e5e5edabf573f46548d393928a608bb
AislinnRay <aislinn.ray.m@gmail.com>
2020-05-10 22:10:49 -0600

    added answers

--- js-competencies/closures/closures.js
+++ js-competencies/closures/closures.js
@@ -34,29 +34,29 @@ function adventure(mode) {
 
 //What data type is the value returned by calling adventure("plane")?
 
-// var adventureReturns = "function"
-// var adventureReturns = "string"
-// var adventureReturns = "undefined"
+var adventureReturns = "function"
+//var adventureReturns = "string"
+//var adventureReturns = "undefined"
 // var adventureReturns = "object"
 
 
 //True or false: "origin" is a public variable contained within "adventure".
 
 // var originIsPublic = true
-// var originIsPublic = false
+var originIsPublic = false
 
 
 //True or false: the function returned from "adventure" is public.
 
-// var functionIsPublic = true
+var functionIsPublic = true
 // var functionIsPublic = false
 
 //True or false: the function returned from "adventure" can access
 //the parameter "mode", and the variable "origin", even after 
 //the function "adventure" has exited.
 
-// var functionCanStillAccess = true
-// var functionCanStillAccess = false
+var functionCanStillAccess = true
+//var functionCanStillAccess = false
 
 
 //////////////////PROBLEM 3////////////////////
@@ -81,39 +81,39 @@ function careerUpdater(name, city, job) {
 //city "Windsor", and job "Lady-In-Waiting". Store the resulting
 //function in a variable called "annePromotion"
 
-//Code here
+let annePromotion = careerUpdater("Anne", "Windsor", "Lady-In-Waiting")
 
 
 //Call annePromotion with the arguments "London", "Queen". Store the 
 //result in a variable called "anneResumeEntry1"
 
-//Code here
+let anneResumeEntry1 = annePromotion("London","Queen")
 
 
 //Then, call annePromotion again with just the argument 
 //"Tower of London dungeons" and save it to a variable called
 //"anneResumeEntry2"
 
-//Code here
+let anneResumeEntry2 = annePromotion("Tower of London dungeons")
 
 
 //Based on the instance of annePromotion, and the lack of a newJob
 //argument, what should Anne's job be after creating anneResumeEntry2? 
 //Uncomment the answer.
 
-// var annesJobInDungeon = "Lady-In-Waiting"
-// var annesJobInDungeon = "Queen"
-// var annesJobInDungeon = "undefined"
+var annesJobInDungeon = "Lady-In-Waiting"
+//var annesJobInDungeon = "Queen"
+//var annesJobInDungeon = "undefined"
 
 
 //Create another instance of careerUpdater, with the arguments
 //"Clara", "Salt Lake City", "student". Store it in a var
 //called "claraPromotion".
 
-//Code here
+let claraPromotion = careerUpdater("Clara", "Salt Lake City", "student")
 
 
 //Then, call claraPromotion with the arguments "Seattle", "programmer"
 //and store it in a variable called "claraResumeEntry"
 
-//Code here
+let claraResumeEntry = claraPromotion("Seattle", "programmer")
